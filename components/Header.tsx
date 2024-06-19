dd6178a2b15ab94e3e3f612efff57d254a59a838
Igor Silva <igorsilvauk@gmail.com>
2024-06-19 11:55:57 +0100

    Image ajusted and header size ajusted

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="flex flex-col gap-16 items-center bg-[url('/imgs/bg.png')] w-screen h-[65vh] bg-cover bg-no-repeat bg-center">
+    <div className="flex flex-col gap-16 items-center bg-[url('/imgs/bg.jpeg')] w-screen h-[60vh] bg-cover bg-no-repeat bg-center">
       Header
     </div>
   );
