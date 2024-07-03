ea8c5ffa2a183a5d5afe05d56f2715a119df5a10
Igor Silva <igorsilvauk@gmail.com>
2024-07-03 09:02:21 +0100

    .

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center bg-[url('/imgs/bg.jpeg')] w-screen h-[60vh] bg-cover bg-no-repeat bg-center text-white">
-      Content / Filter
+      Content
     </div>
   );
 }
