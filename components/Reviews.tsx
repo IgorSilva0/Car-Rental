41023601ab0fd7ea111b2e8231ec6c0ff06dbeee
Igor Silva <igorsilvauk@gmail.com>
2024-08-10 18:47:32 +0100

    Feedback ammount increased

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -4,7 +4,7 @@ const Reviews = () => {
 
     return (
         <div className="flex flex-col items-center gap-5 bg-slate-900 p-5 rounded-md shadow-lg">
-            <h2 className=" bg-white text-black py-1 px-3 rounded-md font-bold">Over 10,000 reviews and counting…</h2>
+            <h2 className=" bg-white text-black py-1 px-3 rounded-md font-bold">Over 100,000 reviews and counting…</h2>
             <p>"Quick, efficient and effective service"</p>
             <Image src={'/imgs/feefo.png'} alt="Feefo image" width={150} height={50}/>
         </div>
